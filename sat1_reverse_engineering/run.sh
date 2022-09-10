e62bf56c91268b8129d971e0cb67af8cb9994517
Brendan Kapp <brendankapp@protonmail.com>
2022-09-10 11:09:21 -0400

    Remove useless C code

--- sat1_reverse_engineering/run.sh
+++
@@ -1,4 +0,0 @@
-# Running docker container
-sudo docker run -p 8001:8001 sat1
-# Building docker container
-sudo docker build . --tag sat1
--- sat1_reverse_engineering/setup.sh
+++
@@ -1,5 +0,0 @@
-gcc -o TigerS tigers.c
-
-gcc -o TigerC client/tigerc.c
-
-mv TigerC client/TigerC
